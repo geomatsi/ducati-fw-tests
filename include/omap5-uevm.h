fccf7208a0582a68a0a9fb74000ed30146c7d2d9
matsi <matsi@example.org>
2014-08-13 10:17:46 -0400

    Modify virtual and physical address assignments for GPIO peripherals
    Modify virtual and physical address assignments for GPIO5 block. We have to
    make it work with simple first-level IPU MMU configuration implemented in
    U-Boot, where each entry addresses 1MB section aligned to 1MB.
    
    Signed-off-by: matsi <matsi@example.org>

--- include/omap5-uevm.h
+++ include/omap5-uevm.h
@@ -4,19 +4,19 @@
 #define IPU_PID_REG         0xE00FFFE0
 
 #define CPU_TEXT_MEM        0xb0000000
-#define CPU_GPIO_MEM        0x48050000
+#define CPU_GPIO_MEM        0x48000000
 
 #define IPU_TEXT_MEM        0x0
 #define IPU_TEXT_SIZE       0x100000
 #define IPU_TEXT_NAME       "text"
 
 #define IPU_GPIO_MEM        0xfff00000
-#define IPU_GPIO_SIZE       0x10000
+#define IPU_GPIO_SIZE       0x100000
 #define IPU_GPIO_NAME       "gpio5"
 
 /* GPIO5 registers mapped to IPU memory */
 
-#define IPU_GPIO_DATAOUT    0xfff0B13c
-#define IPU_GPIO_OE         0xfff0B134
+#define IPU_GPIO_DATAOUT    0xfff5B13c
+#define IPU_GPIO_OE         0xfff5B134
 
 #endif /* OMAP5_UEVM_H */
